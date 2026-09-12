7b52816f40623bdc91e1fddae1384252499def6b
sumitkumards07 <sumitkumards07@gmail.com>
2026-09-12 18:16:44 +0530

    perf: inline css into js to remove render-blocking stylesheet

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
 })
